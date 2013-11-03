f281a5c35040ac999e47d249bac39db1cb46bfee
Daniel C Musigire <dalm05@cs.umb.edu>
2013-11-03 19:36:52 +0300

    version1

---
+++ AAPG/menucontents.js
@@ -0,0 +1,81 @@
+var anylinkmenu1={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu1.items=[
+	["Dynamic Drive", "http://www.dynamicdrive.com/"],
+	["CSS Drive", "http://www.cssdrive.com/"],
+	["JavaScript Kit", "http://www.javascriptkit.com/"],
+	["Coding Forums", "http://www.codingforums.com/"],
+	["JavaScript Reference", "http://www.javascriptkit.com/jsref/"] //no comma following last entry!
+]
+
+
+
+var anylinkmenu2={divclass:'anylinkmenu', inlinestyle:'width:150px; background:#FDD271', linktarget:'_new'} //Second menu variable. Same precaution.
+anylinkmenu2.items=[
+	["Responsibility for Procurement", "302.pdf"],
+	["MSNBC", "http://www.msnbc.com/"],
+	["Google", "http://www.google.com/"],
+	["BBC News", "http://news.bbc.co.uk"] //no comma following last entry!
+]
+
+
+
+var anylinkmenu3={divclass:'anylinkmenucols', inlinestyle:'', linktarget:'secwin'} //Third menu variable. Same precaution.
+anylinkmenu3.cols={divclass:'column', inlinestyle:''} //menu.cols if defined creates columns of menu links segmented by keyword "efc"
+anylinkmenu3.items=[
+	["Dynamic Drive", "http://www.dynamicdrive.com/"],
+	["CSS Drive", "http://www.cssdrive.com/"],
+	["JavaScript Kit", "http://www.javascriptkit.com/"],
+	["Coding Forums", "http://www.codingforums.com/"],
+	["JavaScript Reference", "http://www.javascriptkit.com/jsref/", "efc"],
+	["CNN", "http://www.cnn.com/"],
+	["MSNBC", "http://www.msnbc.com/"],
+	["Google", "http://www.google.com/"],
+	["BBC News", "http://news.bbc.co.uk", "efc"],
+	["News.com", "http://www.news.com/"],
+	["SlashDot", "http://www.slashdot.com/"],
+	["Digg", "http://www.digg.com/"],
+	["Tech Crunch", "http://techcrunch.com"] //no comma following last entry!
+]
+
+var planning={divclass:'anylinkmenu', inlinestyle:'width:300px; background:#DFFDF4', linktarget:'_new'} //Second menu variable. Same precaution.
+planning.items=[
+	["Acqiusition and Assistance Planning", "300.pdf"] //no comma following this entry!
+]
+
+var acquisition ={divclass:'anylinkmenu', inlinestyle:'width:300px; background:#DFFDF4', linktarget:'_new'} //Second menu variable. Same precaution.
+acquisition.items=[
+	["Responsibility for Porcurement", "301.pdf"],
+	["USAID Direct Contracting", "302.pdf"],
+	["Host Country Contracts", "305.pdf"],
+	["Procurement Under Immediate Credit Institutions", "316.pdf"],
+	["Procurement Under Fixed Amount Reimbursement Activities", "317.pdf"],
+	["Post Procurement", "322.pdf"],
+	["Host Country Contracts", "324.pdf"],
+	["Source Rules for Administrative Procurement", "330.pdf"],
+	["Simplified Acquisitions, Micro-purchases, and Use of the USAID Worldwide Purchase Card", "331.pdf"] //no comma following last entry!
+]
+
+var assistance ={divclass:'anylinkmenu', inlinestyle:'width:300px; background:#DFFDF4', linktarget:'_new'} //Second menu variable. Same precaution.
+assistance.items=[
+	["Grants and Cooperative Agreements to Non-Governmental Organizations", "303.pdf"],
+	["Grants to Foreign Governments", "350.pdf"] //no comma following last entry!
+]
+
+var misc ={divclass:'anylinkmenu', inlinestyle:'width:300px; background:#DFFDF4', linktarget:'_new'} //Second menu variable. Same precaution.
+misc.items=[
+	["Selecting Between Acquisition and Assistance (A&A) Implementing Instruments", "304.pdf"],
+	["Interagency Agreements", "306.pdf"],
+	["Commodity Import Programs", "307.pdf"],
+	["Awards to Public International Organizations", "308.pdf"],
+	["Source and Nationality Requirements for Procurement of Commodities and Services Financed by USAID", "310.pdf"],
+	["Eligibility of Commoditiess", "312.pdf"],
+	["Eligibility of Suppliers, Contractors, and Recipients", "313.pdf"],
+	["Eligibility of Delivery Services", "314.pdf"],	
+	["Cargo Preference", "315.pdf"],
+	["Intellectual Property Rights", "318.pdf"],
+	["Branding and Marking", "320.pdf"],
+	["Small Business Programs, Disadvantaged Enterprises, and Minority Serving Institutions", "321.pdf"],
+	["Metric Specifications", "323.pdf"],
+	["International Agreements", "349.pdf"],	
+	["Agreements with Bilateral Donors", "351.pdf"] //no comma following last entry!
+]
